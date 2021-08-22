ac67ff1a2a1d8bec0cfdabbca6fc540e79599e79
andy8421 <yichang8421@qq.com>
2021-08-23 01:26:48 +0800

    connect 更新当前组件，利用 useState.setState

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, {useState, useContext} from "react"
 
 const appContext = React.createContext(null)
 
+const store = {
+    state: {
+        user: {name: "andy8421", age: 18}
+    },
+    setState(newState) {
+        console.log(newState)
+        store.state = newState
+    }
+}
+
 function App() {
-    const [appState, setAppState] = useState({
-        user: {
-            name: "andy8421",
-            age: 18
-        }
-    })
     return (
-        <appContext.Provider value={{appState, setAppState}}>
+        <appContext.Provider value={store}>
             <FirstChild/>
             <SecondChild/>
             <ThirdChild/>
@@ -40,10 +44,10 @@ function ThirdChild() {
 }
 
 const User = () => {
-    const {appState} = useContext(appContext)
+    const {state} = useContext(appContext)
     return (
         <div>
-            User: {appState.user.name}
+            User: {state.user.name}
         </div>
     )
 }
@@ -64,31 +68,36 @@ const reducer = (state, {type, payload}) => {
 
 const connect = (Component) => {
     return (props) => {
-        const {appState, setAppState} = useContext(appContext)
+        const {state, setState} = useContext(appContext)
+        const [, update] = useState({})
         const dispatch = (action) => {
-            setAppState(() => {
-                return reducer(appState, action)
-            })
+            setState(reducer(state, action))
+            // 此处 setState 是 App 的 setState 方法。不是 React 提供的 setState 方法。
+
+            update({})
+            // 调用 App 的 setState 方法后，store 的值发生改变。此时调用 update(),将使当前组件重新渲染。
         }
 
-        return (<Component {...props} dispatch={dispatch} state={appState}/>)
+        return (<Component {...props} dispatch={dispatch} state={state}/>)
     }
 }
 
 const UserModifier = connect(({dispatch, state, children}) => {
+    const onChange = (e) => {
+        dispatch({
+            type: "updeteUser",
+            payload: {
+                name: e.target.value
+            }
+        })
+    }
+
     return (
         <div>
             {children}
             <input
                 value={state.user.name}
-                onChange={(e) => {
-                    dispatch({
-                        type: "updeteUser",
-                        payload: {
-                            name: e.target.value
-                        }
-                    })
-                }}
+                onChange={onChange}
             />
         </div>
     )
